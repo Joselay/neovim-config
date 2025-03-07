4671996e878d76fa6d77fa8ba91082ac73072a13
Smae Tongmenglay <smaetongmenglay@JOSE.local>
2025-03-08 00:33:21 +0700

    updated config

--- README.md
+++
@@ -1,6 +0,0 @@
-💤 LazyVim
-
-A starter template for [LazyVim](https://github.com/LazyVim/LazyVim).
-Refer to the [documentation](https://lazyvim.github.io/installation) to get started. <br/>
-# Dashboard
-![dashboard](https://github.com/Joselay/neovim-config/assets/98274892/fea1eef0-adbf-413f-9c05-065c327a111a)
--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,14 +1,9 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    lazy = true,
-    priority = 1000,
-    opts = { style = "moon" },
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "tokyonight",
-    },
-  }
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {},
+  config = function()
+    vim.cmd([[colorscheme tokyonight]])
+  end,
 }
