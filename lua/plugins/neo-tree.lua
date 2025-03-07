c4ce50c1456d962003bc03639e54882ac0524196
Smae Tongmenglay <smaetongmenglay@JOSE.local>
2025-03-08 01:41:47 +0700

    .

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,7 +7,7 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function()
-    vim.keymap.set('n', '<C-n>', ':Neotree toggle<CR>', {})
-    vim.keymap.set('n', '<leader>e', ':Neotree focus<CR>', {})
-  end
+    vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>", {})
+    vim.keymap.set("n", "<leader>e", ":Neotree focus<CR>", {})
+  end,
 }
